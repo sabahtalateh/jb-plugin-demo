172a026760785b6072bd52ff9f2d2dd5f0a6856e
GitHub Action <action@github.com>
2021-03-25 22:51:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/sabahtalateh/jbplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sabahtalateh/jbplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sabahtalateh.jbplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sabahtalateh.jbplugindemo.MyBundle
 
 class MyApplicationService {
 
